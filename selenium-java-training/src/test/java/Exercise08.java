32136adab4900c5ecfa5bf9c6d478a3f58e4b8cc
ysparrow <ysparrow@gmail.com>
2017-02-01 00:49:41 +0200

    Added Exercise08

---
+++ selenium-java-training/src/test/java/Exercise08.java
@@ -0,0 +1,53 @@
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by sparrow on 31.01.2017.
+ */
+
+public class Exercise08 {
+
+    private WebDriver driver;
+    private String baseURL = "http://localhost:4040/litecart";
+
+    @Before
+    public void setUp() {
+        ChromeDriverManager.getInstance().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+
+    @Test
+    public void homePageItemsStickerTest() {
+
+        driver.navigate().to(baseURL);
+
+        List<WebElement> items = driver.findElements(By.cssSelector("li.product.column.shadow"));
+
+        if (items.size() > 0) {
+            for (WebElement item : items) {
+                Assert.assertTrue("Number of stickers is not equal to 1 for item: " + item.getText(),
+                        item.findElements(By.cssSelector(".sticker")).size() == 1);
+            }
+        }
+    }
+
+    @After
+    public void shutDown() {
+        driver.quit();
+        driver = null;
+    }
+
+}
